491eeef70ce3ba4ba61350e1c2531db023154d9a
Shakir <shaker88999@gmail.com>
2023-08-29 18:39:14 +0530

    updated form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,61 +1,114 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 function Form() {
-  const [formData, setFormData] = useState({
+  const [userFormData, setUserFormData] = useState({
     username: "",
     email: "",
     message: "",
   });
 
+  const successBg = "#46f17f";
+  const failedBg = "#ef4444";
+
+  const confirmSubmission = useRef();
+
+  const handleSubmissionConfirmation = (message, bgColor) => {
+    const element = confirmSubmission.current;
+    element.classList.toggle("hidden");
+    element.textContent = message;
+    element.style.backgroundColor = bgColor;
+
+    setTimeout(() => {
+      element.classList.toggle("hidden");
+    }, 5000);
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    const myForm = e.target;
+    const formData = new FormData(myForm);
+
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-from-urlencoded" },
+      body: new URLSearchParams(formData).toString(),
+    })
+      .then((msg) => {
+        handleSubmissionConfirmation("Message Sent ✓", successBg);
+      })
+      .catch((err) => {
+        handleSubmissionConfirmation("Error Occured..!", failedBg);
+      });
   };
 
   const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
+    setUserFormData({ ...userFormData, [e.target.name]: e.target.value });
   };
 
   return (
     <div className="max-w-[480px] text-[0.85rem] font-bold">
-      <form onSubmit={handleSubmit} className="flex flex-col ">
+      <form
+        className="flex flex-col "
+        name="contact"
+        method="POST"
+        data-netlify="true"
+        onSubmit={handleSubmit}
+      >
         <div className="flex flex-col gap-6">
           <div className=" flex flex-col justify-between sm:flex-row gap-5 sm:gap-10">
-            <label>
+            <label htmlFor="user-name">
               Name
               <br />
               <input
+                id="user-name"
                 type="text"
-                value={formData.username}
+                value={userFormData.username}
                 name="username"
                 onChange={handleChange}
                 className=" mt-1 font-normal w-full px-2 py-1  bg-bg-accent"
               />
             </label>
 
-            <label>
+            <label htmlFor="email">
               Email
               <br />
               <input
+                id="email"
                 type="email"
-                value={formData.email}
+                value={userFormData.email}
                 name="email"
+                required
                 onChange={handleChange}
                 className="mt-1 font-normal w-full px-2 py-1  bg-bg-accent"
               />
             </label>
           </div>
 
-          <label>
+          <label htmlFor="message">
             Message
             <br />
-            <textarea className=" mt-1 font-normal px-2 py-1 w-full h-32 resize-none  bg-bg-accent"></textarea>
+            <textarea
+              id="message"
+              className=" mt-1 font-normal px-2 py-1 w-full h-32 resize-none  bg-bg-accent"
+              name="message"
+              value={userFormData.message}
+              onChange={handleChange}
+            ></textarea>
           </label>
         </div>
-        <input
-          type="submit"
-          value="send message"
-          className="px-6 py-2 mt-3 self-end sm:self-center border border-accent cursor-pointer hover:bg-btn-hover"
-        />
+        <div className="relative w-fit mt-3 self-end sm:self-center">
+          <input
+            type="submit"
+            value="send message"
+            className="px-6 py-2 border 
+          border-accent cursor-pointer hover:bg-btn-hover"
+          />
+          <div
+            ref={confirmSubmission}
+            className={` absolute hidden inset-0 px-4 py-2 `}
+          ></div>
+        </div>
       </form>
     </div>
   );
